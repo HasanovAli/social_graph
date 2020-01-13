25a563386afd7b9048b514326e71fb0729780cce
ahasa <ahasa@softserveinc.com>
2020-01-13 14:17:12 +0200

    add opportunity to like pictures

--- images/templates/bookmarklet_launcher.js
+++ images/templates/bookmarklet_launcher.js
@@ -3,9 +3,6 @@
         myBookmarklet();
     }
     else {
-    document.body.appendChild(
-        document.createElement('script')
-    ).src='https://9ebfacfd.ngrok.io/static/js/bookmarklet.js?r=' +
-        Math.floor(Math.random()*99999999999999999999);
+    document.body.appendChild(document.createElement('script')).src='https://9ebfacfd.ngrok.io/static/js/bookmarklet.js?r=' + Math.floor(Math.random()*99999999999999999999);
     }
 })();
